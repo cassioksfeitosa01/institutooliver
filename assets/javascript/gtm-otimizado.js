d89fed615341bdc3bbc14ff053749feb027a8f28
cassioksfeitosa01 <cassiodossantosfeitosa.anucio@gmail.com>
2026-06-02 17:28:49 -0300

    atualização nos aquivos

--- assets/javascript/gtm-otimizado.js
+++ assets/javascript/gtm-otimizado.js
@@ -6,24 +6,33 @@
         if (carregado) return; 
         carregado = true;
 
+        // Remove os ouvintes imediatamente
         window.removeEventListener('scroll', dispararGTM);
         window.removeEventListener('mousemove', dispararGTM);
         window.removeEventListener('touchstart', dispararGTM);
 
-        (function (w, d, s, l, i) {
-            w[l] = w[l] || []; w[l].push({
-                'gtm.start': new Date().getTime(), event: 'gtm.js'
-            }); var f = d.getElementsByTagName(s)[0],
-                j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src =
-                    'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
-        })(window, document, 'script', 'dataLayer', gtmId);
+        // Usa requestIdleCallback para rodar o GTM quando o navegador estiver totalmente livre
+        var executarScript = function() {
+            (function (w, d, s, l, i) {
+                w[l] = w[l] || []; w[l].push({
+                    'gtm.start': new Date().getTime(), event: 'gtm.js'
+                }); var f = d.getElementsByTagName(s)[0],
+                    j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src =
+                        'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
+            })(window, document, 'script', 'dataLayer', gtmId);
+            
+            console.log("🚀 GTM ativado em segundo plano!");
+        };
 
-        console.log("🚀 GTM ativado!");
+        if ('requestIdleCallback' in window) {
+            requestIdleCallback(executarScript, { timeout: 2000 });
+        } else {
+            setTimeout(executarScript, 100);
+        }
     }
 
+    // Dispara apenas por ação real do usuário (removemos o setTimeout de 3 segundos antigo)
     window.addEventListener('scroll', dispararGTM, { passive: true });
     window.addEventListener('mousemove', dispararGTM);
     window.addEventListener('touchstart', dispararGTM, { passive: true });
-
-    setTimeout(dispararGTM, 3000); 
 })();
